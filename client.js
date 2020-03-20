6934cd0190ed4216672411b00b62e95c032c182b
Pamela Anderson <cyberryder2010@gmail.com>
2020-03-20 15:08:21 -0500

    new task function

--- client.js
+++ client.js
@@ -12,11 +12,19 @@ function init() {
 
 function addTaskToList(event) {
   console.log("Task Added");
+
   const myTodoList = {
-    name: $("js-input-newTask").val(),
-    completed: $("js-btn-completed").val()
+    name: $(".js-input-newTask").val(),
+    completed: false
   };
 
   tasks.push(myTodoList);
   console.log("ToDo List:", tasks);
+
+  $(".js-input-newTask").val("");
+  render();
+}
+
+function render() {
+  console.log("Render");
 }
